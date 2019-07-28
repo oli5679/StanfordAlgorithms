5a9cead8392cc9cb1270aa8ba5a50b291d001a76
oli5679 <olicairns93@gmail.com>
2019-07-28 16:06:42 +0100

    update week 5

--- course_1/scripts/ps_5.py
+++ course_1/scripts/ps_5.py
@@ -2,67 +2,93 @@ from IPython import embed
 from copy import copy
 import numpy
 
+INPUT_PATH = "../data/dijkstraData.txt"
 
-def dijkstra(paths, start):
-    """
-    Takes as input a list of undirected paths, a 'start' vertex and val for unconnected verteces.
-    Returns shortest distance from start to all verteces (does not handle negative edges)
-    """
-    # Setup - only start explored. Distance of 0.
-    len_paths = len(paths)
-    unexplored = list(range(len_paths))
-    distances = [100000000] * len_paths
-    unexplored.pop(start)
-    explored = [start]
-    distances[start] = 0
-    # Crossers = paths between explored and not.
-    # Key = destination. Value = distance from start.
-    crossers = {path[0]: path[1] for path in paths[start]}
 
-    for _ in range(len_paths - 1):
+class Dijkstra:
+    def __init__(self, paths, start):
+        self.paths = paths
+        self.start = start
+        """
+        Takes as input a list of undirected paths, a 'start' vertex and val for unconnected verteces.
+        Returns shortest distance from start to all verteces (does not handle negative edges)
+        """
+        # Setup - only start explored. Distance of 0.
+        self.len_paths = len(paths)
+        self.unexplored = list(range(self.len_paths))
+        self.distances = [100000000] * self.len_paths
+        self.unexplored.pop(start)
+        self.explored = [start]
+        self.distances[start] = 0
 
+        # Crossers = paths between explored and not.
+        # Key = destination. Value = distance from start.
+        self.crossers = {path[0]: path[1] for path in paths[start]}
+
+    def solve(self):
+        for _ in range(self.len_paths - 1):
+            chosen, chosen_val = self.choose_min_crosser()
+            self.update_paths(chosen, chosen_val)
+
+        return self.distances
+
+    def choose_min_crosser(self):
         # Choose lowest value crosser
-        chosen = min(crossers, key=crossers.get)
+        chosen = min(self.crossers, key=self.crossers.get)
 
         # Also find its value
-
-        chosen_val = crossers[chosen]
+        chosen_val = self.crossers[chosen]
 
         # Remove from 'unexplored' and add to 'explored'.
-        unexplored.remove(chosen)
-        explored.append(chosen)
+        self.unexplored.remove(chosen)
+        self.explored.append(chosen)
 
         # Update distance and remove entry from 'crossers'.
-        distances[chosen] = crossers[chosen]
-        del crossers[chosen]
+        self.distances[chosen] = self.crossers[chosen]
+        del self.crossers[chosen]
 
+        return chosen, chosen_val
+
+    def update_paths(self, chosen, chosen_val):
         # Look at paths leading from newly included vertex.
-        for path in paths[chosen]:
+        for path in self.paths[chosen]:
 
             # If path leads to unexplored other vertex.
-            if path[0] in unexplored:
+            if path[0] in self.unexplored:
 
                 # Add/update minimum distance for this vertex.
                 path_val = chosen_val + path[1]
-                if path[0] in crossers:
-                    crossers[path[0]] = min(path_val, crossers[path[0]])
+                if path[0] in self.crossers:
+                    self.crossers[path[0]] = min(
+                        path_val, self.crossers[path[0]])
                 else:
-                    crossers[path[0]] = path_val
-    return distances
+                    self.crossers[path[0]] = path_val
 
 
-# Data processing
-TXT_FILE = open("dijkstraData.txt", "r")
-INPUT_STRING = TXT_FILE.read()
-ROWS = INPUT_STRING.splitlines()
-PATHS = [row.split("\t") for row in ROWS]
-PATHS = [[[int(v) for v in path.split(",")] for path in row[1:-1]] for row in PATHS]
-PATHS = [[[path[0] - 1, path[1]] for path in vertex] for vertex in PATHS]
+def parse_file(file_path):
+    rows = open(file_path, "r").read().splitlines()
+    paths = [row.split("\t") for row in rows]
+    paths = [[[int(v) for v in path.split(",")]
+              for path in row[1:-1]] for row in paths]
+    return [[[path[0] - 1, path[1]] for path in vertex] for vertex in paths]
 
 
-# Core algorithm
-DISTANCES = dijkstra(paths=PATHS, start=0)
+def main():
+    paths = parse_file(INPUT_PATH)
+    shortest_path_finder = Dijkstra(paths=paths, start=0)
+    shortest_path_finder.solve()
 
-# Answers
-for num in [6, 36, 58, 81, 98, 114, 132, 164, 187, 196]:
-    print("number {} - distance {}".format(num, DISTANCES[num]))
+    # Answers
+    for num in [6, 36, 58, 81, 98, 114, 132, 164, 187, 196]:
+        print(
+            f"number {num+1} - distance {shortest_path_finder.distances[num]}")
+
+    assert shortest_path_finder.distances[6] == 2599
+
+    assert shortest_path_finder.distances[36] == 2610
+
+    assert shortest_path_finder.distances[98] == 2367
+
+
+if __name__ == "__main__":
+    main()
